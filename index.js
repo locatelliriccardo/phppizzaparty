087a2b51acf2d5d2a39f68b1f9259221e99c939b
fregoni1312 <33417641+fregoni1312@users.noreply.github.com>
2017-11-07 11:22:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
-var express = require('express');
-var app = express();
+<HTML>  
+  Scritta indentata seguita da un break
+  il break mi ha portato a riga nuova
+  il paragrafo stacca di piu' (e permette di cambiare stile)
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+</HTML>
