651c78ebef55bc46eaa859cca0d3128c2fb67a8b
locatelliriccardo <33417625+locatelliriccardo@users.noreply.github.com>
2017-12-11 19:57:16 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -7,7 +7,7 @@
                 background: #6b6b47;
                 font-family: sans-serif;   
             }
-        	.login-div{
+       	    .login-div{
                 width: 460px;
                 padding: 8% 0 0;
                 margin: auto;
@@ -24,7 +24,7 @@
         	    margin: 2px;
                 padding: 0;
         	}
-        	.login-form input[type="text"], input[type="password"], input[type="submit"]{
+        	.login-form input[type="text"], input[type="password"], input[type="submit"],input[type="button"]{
                 font-family: sans-serif;
                 background: #ede9e1;
                 width: 100%;
@@ -47,6 +47,19 @@
                 margin-left: auto;
                 margin-right: auto 
         	}
+		table {
+		   border-collapse: collapse;
+		   width: 100%;
+		}
+		tr:nth-child(even){background-color: #ece8df}
+		th {
+		   background-color: #d9d2bf;
+		   color: white;
+		}
+		th, td {
+		   text-align: left;
+		   padding: 8px;
+		}
 		</style>
 		<script>
 			function controllo_campi()
@@ -75,23 +88,16 @@
 				$que=$_POST["que"];
 			else
 				$que="pizzeria";
-			echo ("<div class=
-			echo
-			echo "<form id='forma' method='post' onsubmit='return controllo_campi();'><br/>";
-			echo "<table>";
-			echo "<tr>";
-			echo " <td>Numero elementi (1-50): </td><td><input type='text' value='$lim' name='lim'id='lim' /></td>";
-			echo "</tr>";
-			echo "<tr>";
-			echo " <td>Citta: </td><td><input type='text' value='$cit' name='cit' id='cit' /></td>";
-			echo "</tr>";
-			echo "<tr>";
-			echo " <td>Cosa stai cercando?: </td><td><input type='text' value='$que' name='que' id='que' /></td><br/>";
-			echo "</tr>";
-			echo "</table>";
-			echo " <input type='submit' value='Aggiorna tabella' class='btn'/>";
-			echo "</form>";
-		
+			echo ("<div class='login-div'>");
+                         echo ("<div class='login-form'>");
+			  echo ("<form id='forma' method='post' onsubmit='return controllo_campi();'><br/>");
+			     echo ("Numero elementi (1-50) <input type='text' value='$lim' name='lim'id='lim' />");
+			     echo ("Citta <input type='text' value='$cit' name='cit' id='cit' />");
+			     echo ("Cosa stai cercando </td><td><input type='text' value='$que' name='que' id='que' />");
+			   echo ("<input type='submit' value='Aggiorna tabella' />");
+			  echo ("</form>");
+			 echo("</div>");
+			echo("</div>");
 			# Questo script chiama un'API e la inserisce in una tabella 
 			# Indirizzo dell'API da richiedere
 		        $indirizzo_pagina="https://api.foursquare.com/v2/venues/search?v=20161016&query=$que&limit=$lim&intent=checkin&client_id=4DLLUZVXJEQIL0DFCN3B3YFG4EN4W4DMICUVPSNMRD24XKVU&W&client_secret=ZWWMV4LSNXGTZIRIUWHGE5PQDESQ0AHBACUPXVDPTESUTLRX&near=$cit";
@@ -104,27 +110,25 @@
 			# Decodifico la stringa json e la salvo nella variabile $data
 			$data = json_decode($json);
 			# Stampa della tabella delle pizzerie.
-			echo "<table>";
-				echo "<tr>";
-					echo "<th>NOME</th>";
-					echo "<th>LATITUDINE</th>";
-					echo "<th>LONGITUDINE</th>";
-				echo "</tr>";
+			echo ("<div class='login-div'>");
+                         echo ("<div class='login-form'>");
+			  echo ("<table>");
+				echo("<tr>");
+				 echo ("<th>NOME</th>");
+				 echo ("<th>LATITUDINE</th>");
+				 echo ("<th>LONGITUDINE</th>");
+				echo "(</tr>");
 				for($i=0; $i<$lim; $i++)
 				{	
-					echo "<tr>";
-					echo "<td>";
-					echo $data->response->venues[$i]->name;
-					echo "</td>";
-					echo "<td>";
-					echo $data->response->venues[$i]->location->lat;
-					echo "</td>";
-					echo "<td>";
-					echo $data->response->venues[$i]->location->lng;
-					echo "</td>";
-					echo "</tr>";
+					echo ("<tr>");
+					echo ("<td>".$data->response->venues[$i]->name."</td>");
+					echo ("<td>".$data->response->venues[$i]->location->lat."</td>");
+					echo ("<td>".$data->response->venues[$i]->location->lng."</td>");
+					echo ("</tr>");
 				}
-			echo "</table>";
+			  echo ("</table>");
+			 echo("</div>");
+			echo("</div>");
 			# Stampa di eventuali errori
 			echo curl_error($ch);
 			curl_close($ch);
