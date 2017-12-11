37783e2860bf8d4939b6f4a13d6ca09c1ee3f7b4
locatelliriccardo <33417625+locatelliriccardo@users.noreply.github.com>
2017-12-11 21:08:34 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -4,43 +4,36 @@
 		<title>cose</title>
 		<link rel="shortcut icon" type="image/x-icon" href="images/l.png" />
 		<link rel="stylesheet" type="text/css" href="style.css">
-		<script>
-			function controllo_campi()
-			{
-				var valore=document.getElementById("lim").value;
-				var esito=false;
-				var verifica=/^\d{1,2}$/
-				if(document.getElementById("lim").value!=""&&document.getElementById("cit").value!=""&&document.getElementById("que").value!="")
-					if(valore.match(verifica)&&parseInt(valore)<51)
-						esito=true;
-				return esito;
-			}
-		</script>
+
 	</head>
 	<body>
-		<?php
+		<? php
 			if(isset($_POST["lim"]))
-				$lim=$_POST["lim"];
+			{	$lim=$_POST["lim"];}
 			else
-				$lim=10;
+			{	$lim=10;}
 			if(isset($_POST["cit"]))
-				$cit=$_POST["cit"];
+			{	$cit=$_POST["cit"];}
 			else
-				$cit="bergamo";
+			{	$cit="bergamo";}
 			if(isset($_POST["que"]))
-				$que=$_POST["que"];
+			{	$que=$_POST["que"];}
 			else
-				$que="pizzeria";
+			{	$que="pizzeria";}
 			echo ("<div class='login-div'>");
                          echo ("<div class='login-form'>");
 			  echo ("<form id='forma' method='post' onsubmit='return controllo_campi();'><br/>");
-			     echo ("Numero elementi (1-50) <input type='text' value='$lim' name='lim'id='lim' />");
-			     echo ("Citta <input type='text' value='$cit' name='cit' id='cit' />");
-			     echo ("Cosa stai cercando <input type='text' value='$que' name='que' id='que' />");
-			   echo ("<input type='submit' value='Aggiorna tabella' />");
+			     echo("<select>");
+    				for ($i=1; $i<=100; $i++)
+            			 echo("<option value=".$i.">".$i."</option>");
+			     echo("</select>");
+			     //echo ("Numero elementi (1-50) <input type='text' value='$lim' name='lim'id='lim'/>");
+			     echo ("Città <input type='text' value='$cit' name='cit' id='cit'/>");
+			     echo ("Cosa stai cercando <input type='text' value='$que' name='que' id='que'/>");
+			   echo ("<input type='submit' value='Aggiorna tabella'/>");
 			  echo ("</form>");
 			 echo("</div>");
-			echo("</div></br></br></br></br></br>");
+			echo("</div><br/><br/><br/>");
 			# Questo script chiama un'API e la inserisce in una tabella 
 			# Indirizzo dell'API da richiedere
 		        $indirizzo_pagina="https://api.foursquare.com/v2/venues/search?v=20161016&query=$que&limit=$lim&intent=checkin&client_id=4DLLUZVXJEQIL0DFCN3B3YFG4EN4W4DMICUVPSNMRD24XKVU&W&client_secret=ZWWMV4LSNXGTZIRIUWHGE5PQDESQ0AHBACUPXVDPTESUTLRX&near=$cit";
